6e14a605b07391cbce5de58c5d9745143ca814ea
Johan <johandanielbothma@gmail.com>
2023-09-07 10:31:12 +0200

    Setup chatbot py and basic run

--- README.md
+++ README.md
@@ -4,9 +4,9 @@
 
 *https://www.youtube.com/watch?v=t933Gh5fNrc&t=1181s*
 
-# Explaining the model
+# Training.py - Explaining the model
 
-## Libraries
+### Libraries
 
 Below are the libraries we use and what the purpose of each is:
 
@@ -23,9 +23,49 @@ Below are the libraries we use and what the purpose of each is:
 * **import tensorflow as tf** - importing tensorflow for the methods
 * **import nltk** - importing nltk for the methods
 
-## Loading and Preprocessing Data
+### Loading and Preprocessing Data
 
 The chatbot needs to learn from examples of user queries and their corresponding intents(tags). The intents.json file has these examples.
 
 The words in each pattern needs to be preprocessed for consistency and effective learning. We use nltk word_tokenize to tokenize the patterns into individual words, lemmatization to reduce words, and converting to lower case to ensure uniformity. 
 
+### Building Vocabulary and Labels
+
+To train a model, the chatbot needs to understand the words used in the patterns and their corresponding intents (tags). This is why unique words are collected into *words* and unique tags are collected into *classes*.
+
+*Documents* stores patterns with their corresponding tags, forming a basis for building the training dataset. 
+
+### Creating Bag-of-words Representation
+
+Since a MLM requires numerical input, we need to convert the text pattern to a format that it will understand, thus we use the bag-of-words representation. 
+
+In the bag-of-words representation, we set each word in the vocabulary to be a feature. A feature vector is created for each pattern, where each entry indicates whether the corresponding word appears in the pattern or not, represented by a bool.
+
+### Preparing Training Data and Labels
+
+We have input features *train_x* and corresponding output labels *train_y*.
+
+*train_x* is the bag-of-words representation to allow the model to learn the relationships between words and inputs.
+
+*train_y* is the one-hot encoded labels, where each label has a corresponding class index, set to 1, and the rest are 0. This helps the model understand which intent corresponds to each pattern.
+
+### Defining the Neural Network Model
+
+We use TensorFlow's Keras API to create the neural network. The model consists of input, hidden and output layers:
+	1. The input layer size matches the length of the bag-of-words representation.
+	2. The hidden layer allows the model to learn the intermediate representations.
+	3. The output layer has many neurons as there are unique classes, and it uses softmax activation to provide a probability distribution over the classes.
+
+Dropout layers help mitigate overfitting by randomly dropping a fraction of neurons during training.
+
+### Compiling the model
+
+We need to compile the model, which is done by specifying the loss function, optimizer and evaluation metrics. 'categorical_crossentropy' is the chosen loss function as it handles multi-class classification problems. *Stochastic Gradient Descent* (sgd) is an optimization algorithm that updates the model's weights based on the gradien of the loss function. Accuracy is the metric used.
+
+### Traing the model
+
+We train the model by using the *fit()* method. We provide the training data (*train_x*) and labels (*train_y*), along with the training epochs and batch size. The model will learn to adjust its weights to minimize the loss function. The optimization process involves backpropagation and gradient descent.
+
+### Saving the model
+
+Once the model has been trained, we save it to avoid retraining.
--- training.py
+++ training.py
@@ -21,23 +21,23 @@ words = []
 classes = []
 documents = []
 # Add ignore symbols
-ignoreSymbols = ['?', '!', '.', ',']
+ignore_symbols = ['?', '!', '.', ',']
 
 # Loop through all intents and building vocabulary
 for intent in intents['intents']:
 	for pattern in intent['patterns']:
 		# Tokenize the pattern to words in a list
-		wordList = nltk.word_tokenize(pattern)
+		word_list = nltk.word_tokenize(pattern)
 		# Add each word from the list to the words list
-		words.extend(wordList)
+		words.extend(word_list)
 		# Add tuple of world list to documents
-		documents.append([wordList, intent['tag']])
+		documents.append([word_list, intent['tag']])
 		# If the current tag is not in the classes list, add it
 		if intent['tag'] not in classes:
 			classes.append(intent['tag'])
 
 # Take list of words, lemmatizes and creates a new list that excludes the list of symbols
-words = [lemmatizer.lemmatize(word) for word in words if word not in ignoreSymbols]
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_symbols]
 
 # Sort words alphabetically and remove duplicates
 words = sorted(set(words))
@@ -50,37 +50,37 @@ pickle.dump(words, open('words.pkl', 'wb'))
 pickle.dump(classes, open('classes.pkl', 'wb'))
 
 training = []
-outputEmpty = [0] * len(classes)
+output_empty = [0] * len(classes)
 
 # Create bag-of-words representation for each document
 # Loop through the documents which composes out of patterns and their tags to create bag-of-word representation
 for document in documents:
 	# We take the index of the pattern of words from the current document and lemmatize it to a lower string
 	bag = []
-	wordPatterns = document[0]
-	wordPatterns = [lemmatizer.lemmatize(word.lower()) for word in wordPatterns]
+	word_patterns = document[0]
+	word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
 	# Create a binary feature vector of bag
-	# If this word is in wordPatterns, add 1 to the bag, else 0
+	# If this word is in word_patterns, add 1 to the bag, else 0
 	# For example, if the word is `Thank` then it would match the pattern of `Thank you` in the tag `thanks`
 	for word in words:
-		if word in wordPatterns:
+		if word in word_patterns:
 			bag.append(1) 
 		else:
 			bag.append(0)
-	# Create a row that is the same length of the outputEmpty list
-	outputRow = list(outputEmpty)
+	# Create a row that is the same length of the output_empty list
+	output_row = list(output_empty)
 	# Find where the current document's tag is in the classes list
-	outputRow[classes.index(document[1])] = 1
-	training.append(bag + outputRow)
+	output_row[classes.index(document[1])] = 1
+	training.append(bag + output_row)
 
 # Randomize the training and create a numpy array from the training data
 random.shuffle(training)
 training = np.array(training)
 
 # Slice training by only the first len(words) col
-trainX = training[:, :len(words)]
+train_x = training[:, :len(words)]
 # Slice training by starting from the column at index len(words) until the end of the sequence
-trainY = training[:, len(words):]
+train_y = training[:, len(words):]
 
 # Create Neural Network Model
 # Two hidden layers with dropout for regularization
@@ -88,18 +88,18 @@ trainY = training[:, len(words):]
 # Model is compiled with the categorical cross-entropy loss and the stochastic gradient descent optimizer (SGD)
 model = tf.keras.Sequential()
 
-model.add(tf.keras.layers.Dense(128, input_shape=(len(trainX[0]),), activation = 'relu'))
+model.add(tf.keras.layers.Dense(128, input_shape=(len(train_x[0]),), activation = 'relu'))
 # Adjust overfitting
 model.add(tf.keras.layers.Dropout(0.5))
 model.add(tf.keras.layers.Dense(64, activation = 'relu'))
 model.add(tf.keras.layers.Dropout(0.5))
-model.add(tf.keras.layers.Dense(len(trainY[0]), activation='softmax'))
+model.add(tf.keras.layers.Dense(len(train_y[0]), activation='softmax'))
 
 sgd = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-# Train the model with the training data and labels. Trained 200 times in a batch size of 1. Print progress and metrics with verbosity
-hist = model.fit(np.array(trainX), np.array(trainY), epochs=200, batch_size=5, verbose=1)
+# Train the model with the training data and labels. Trained 200 times in a batch size of 5. Print progress and metrics with verbosity
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
 # Save trained model
 model.save('chatbot_model.h5', hist)
 
